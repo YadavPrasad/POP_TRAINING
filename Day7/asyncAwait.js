e05cf4355b3ede5f72d3e8e1d7806bc9806687a2
YadavPrasad <yadavprasad.exe@.gmail.com>
2025-02-18 14:13:45 +0530

    Day8 FN Commit

--- Day7/asyncAwait.js
+++ Day7/asyncAwait.js
@@ -1,11 +1,40 @@
-async function asAw(){
-    console.log("Hello");
-}
-console.log(asAw());
+// async function asAw(){
+//     console.log("Hello");
+// }
 
-async function asAw(){
-    setTimeout(function(){
-        console.log("hello");
-    }, 5000)
+// async function asAw(){
+//     var a = 15
+//     setTimeout(function(){
+//         console.log("hello");
+//     }, 5000)
+//     console.log(await a);
+// }
+// console.log(asAw());
+// asAw().then((res) =>{
+//     console.log(res);
+// }).catch((res) => {
+//     console.log("res");
+// })
+
+//console.log(await asAw());
+
+//Instagram console Demo for post, like, comment, share
+
+
+async function postCode(){
+    var post = new Promise((createPost) => {
+        createPost("Post created successfully")
+    })
+    console.log(await post);
+    console.log(await likeCode);
 }
-console.log(asAw());
+
+postCode()
+
+likeCode = () => {
+    return new Promise((likePost) => {
+        setTimeout(() => {
+            likePost("Post liked successfully");
+        })
+    })
+}
